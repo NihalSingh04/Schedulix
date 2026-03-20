c8d68c3c93db7ac129a942d749662e9cff262539
Nihal Singh <nihalsingh@MacBook-Air.local>
2026-03-21 00:20:33 +0530

    changes made are pushed

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://schedio-backend.onrender.com";
+const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket", "polling"], // ✅ FIXED
